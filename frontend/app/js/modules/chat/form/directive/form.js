cd77698ed13fcaad3180bd5c4ece25f0f3501955
kbarabash <barkir89@gmail.com>
2015-06-02 17:43:36 +0300

    add form

--- frontend/app/js/modules/chat/form/directive/form.js
+++ frontend/app/js/modules/chat/form/directive/form.js
@@ -1,7 +1,17 @@
 (function() {
     'use strict';
 
-    function ChatFormCtrl( ) {
+    function ChatFormCtrl() {
+        this.model = {
+            message: ''
+        };
+
+        this.sendMessage = function() {
+            if (!this.model.message) {
+                return;
+            }
+            console.log(this.model.message);
+        };
     }
 
     function ChatFormDirective($templateCache) {
