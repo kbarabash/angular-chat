3cb0c1fca7dcae33beb109c449ea4b8badf176a7
kbarabash <barkir89@gmail.com>
2015-06-03 15:48:32 +0300

    fix

--- frontend/app/js/modules/chat/provider/config/config.js
+++ frontend/app/js/modules/chat/provider/config/config.js
@@ -1,7 +1,7 @@
 (function() {
     angular.module('chat.provider')
         .constant('CHAT_PROVIDER_CONFIG', {
-            url: 'ws://localhost:8080/',
+            url: 'ws://172.30.227.39:8080/',
             isUseEchoProtocol: true
         });
 })();
